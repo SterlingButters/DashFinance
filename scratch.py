709447032010168b3a9b4594f57cea4d91efae45
SterlingButters <sterlingbutters@gmail.com>
2019-01-15 20:15:15 -0500

    tabs

--- scratch.py
+++ scratch.py
@@ -1,5 +1,5 @@
 from alpha_vantage.timeseries import TimeSeries
-from alpha_vantage.alphavantage import AlphaVantage
+from alpha_vantage.techindicators import TechIndicators
 
 import matplotlib.pyplot as plt
 from pprint import pprint
@@ -12,7 +12,7 @@ import plotly.graph_objs as go
 
 # ONLY 5 API CALLS AT A TIME
 ts = TimeSeries(key='9IDB37CDHYIC07UE', output_format='pandas', indexing_type='integer')
-av = AlphaVantage(key='9IDB37CDHYIC07UE', output_format='pandas', indexing_type='integer')
+ti = TechIndicators(key='9IDB37CDHYIC07UE', output_format='pandas', indexing_type='integer')
 
 # data1, meta_data1 = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
 # data2, meta_data2 = ts.get_intraday(symbol='MSFT',interval='5min', outputsize='full')
@@ -33,30 +33,69 @@ av = AlphaVantage(key='9IDB37CDHYIC07UE', output_format='pandas', indexing_type=
 # pprint(data6)
 # pprint(data1[data1['date'].str.contains("2019-01-14")])
 
-# data6['4. close'].plot()
-# plt.title('Intraday Times Series for the MSFT stock (5 min)')
-
+# get_bbands(self, symbol, interval='daily', time_period=20, series_type='close',
+#                nbdevup=None, nbdevdn=None, matype=None):
+#
+#  """ Return the bollinger bands values in two
+#     json objects as data and meta_data. It raises ValueError when problems
+#     arise
+#     Keyword Arguments:
+#         symbol:  the symbol for the equity we want to get its data
+#         interval:  time interval between two consecutive values,
+#             supported values are '1min', '5min', '15min', '30min', '60min', 'daily',
+#             'weekly', 'monthly' (default 'daily'
+#         time_period: Number of data points used to calculate each BBANDS value.
+#             Positive integers are accepted (e.g., time_period=60, time_period=200)
+#         series_type:  The desired price type in the time series. Four types
+#             are supported: 'close', 'open', 'high', 'low' (default 'close')
+#         nbdevup:  The standard deviation multiplier of the upper band. Positive
+#             integers are accepted as default (default=2)
+#         nbdevdn:  The standard deviation multiplier of the lower band. Positive
+#             integers are accepted as default (default=2)
+#         matype :  Moving average type. By default, matype=0.
+#             Integers 0 - 8 are accepted (check  down the mappings) or the string
+#             containing the math type can also be used.
+#             * 0 = Simple Moving Average (SMA),
+#             * 1 = Exponential Moving Average (EMA),
+#             * 2 = Weighted Moving Average (WMA),
+#             * 3 = Double Exponential Moving Average (DEMA),
+#             * 4 = Triple Exponential Moving Average (TEMA),
+#             * 5 = Triangular Moving Average (TRIMA),
+#             * 6 = T3 Moving Average,
+#             * 7 = Kaufman Adaptive Moving Average (KAMA),
+#             * 8 = MESA Adaptive Moving Average (MAMA)
+#     """
 
 def nearest(items, pivot):
     return min(items, key=lambda x: abs(x - dt.strptime(pivot, '%Y-%m-%d')))
 # dt.strptime(x, '%Y-%m-%d')
 
+ticker = 'AAPL'
+startdate = '2018-01-14'
+enddate = '2019-01-14'
 
-data, meta_data = ts.get_daily_adjusted(symbol='MSFT', outputsize='full')
+data1, meta1 = ts.get_daily_adjusted(symbol='{}'.format(ticker), outputsize='full')
+data2, meta2 = ti.get_bbands(symbol='{}'.format(ticker), interval='daily', time_period=60,
+                             series_type='close',
+                             nbdevup=None, nbdevdn=None, matype=None)
+data1 = data1.reset_index()
+data2 = data2.reset_index()
 
-print(data)
+data1 = data1.loc[(data1['date'] >= startdate) & (data1['date'] < enddate)]
+data2 = data2.loc[(data2['date'] >= startdate) & (data2['date'] < enddate)]
 
-earlier = '1998-01-10'
-later = '2018-12-01'
+period = data1.merge(data2, on='date', how='left')
 
+print(period)
+
+# earlier = '1998-01-10'
+# later = '2018-12-01'
+#
 # period = data.loc[(data['date'] >= earlier) & (data['date'] < later)]
 # data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d',)
-#
-# trace = go.Ohlc(x=period['date'],
-#                 open=period['1. open'],
-#                 high=period['2. high'],
-#                 low=period['3. low'],
-#                 close=period['4. close'])
+
+# trace = go.Scatter(x=period['date'],
+#                    y=period[''])
 # data = [trace]
 # layout = {
 #     'title': 'The Great Recession',
